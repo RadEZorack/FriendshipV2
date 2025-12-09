db8bb18b46f6096ea1258c7f9290bc7c700c0c22
Travis Miller <radezorack@gmail.com>
2025-12-08 19:53:35 -0500

    Initial Commit

---
+++ FriendshipV2/FriendshipV2App.swift
@@ -0,0 +1,17 @@
+//
+//  FriendshipV2App.swift
+//  FriendshipV2
+//
+//  Created by Travis Miller on 2025-12-08.
+//
+
+import SwiftUI
+
+@main
+struct FriendshipV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendshipV2Tests/FriendshipV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendshipV2Tests.swift
+//  FriendshipV2Tests
+//
+//  Created by Travis Miller on 2025-12-08.
+//
+
+import Testing
+@testable import FriendshipV2
+
+struct FriendshipV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
